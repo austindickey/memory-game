927a0a24108362ac7d6451d79e24dbcd9e8d5853
austindickey <austindickey@me.com>
2020-04-30 05:30:45 -0500

    deleting unused files

--- game/src/App.js
+++ game/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import ImageGrid from './components/ImageGrid';
 
 function App() {
--- game/src/index.js
+++ game/src/index.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import Navbar from './components/Navbar'
 import Jumbotron from './components/Jumbotron'
 import Footer from './components/Footer'
 import App from './App'
-import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,5 +14,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
-
-serviceWorker.unregister()

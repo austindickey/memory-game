80ce89bb87cc341097d755d5b34eaba3bbe213c2
austindickey <austindickey@me.com>
2020-05-01 14:42:57 -0500

    changing the picture links to the local directory instead of using url's

--- game/src/components/ImageGrid.js
+++ game/src/components/ImageGrid.js
@@ -8,62 +8,62 @@ const images = [
     {
         id: 1,
         name: "Black Panther",
-        backgroundImage: 'https://github.com/austindickey/memory-game/blob/master/game/public/img/superheros/black-panther.jpg?raw=true'
+        pic: "/img/superheros/black-panther.jpg"
     },
     {
         id: 2,
         name: "Black Widow",
-        backgroundImage: 'https://github.com/austindickey/memory-game/blob/master/game/public/img/superheros/black-widow.jpg?raw=true'
+        pic: "/img/superheros/black-widow.jpg"
     },
     {
         id: 3,
         name: "Captain America",
-        backgroundImage: 'https://github.com/austindickey/memory-game/blob/master/game/public/img/superheros/captain-america.jpg?raw=true'
+        pic: "/img/superheros/captain-america.jpg"
     },
     {
         id: 4,
         name: "Doctor Strange",
-        backgroundImage: 'https://github.com/austindickey/memory-game/blob/master/game/public/img/superheros/doctor-strange.jpg?raw=true'
+        pic: "/img/superheros/doctor-strange.jpg"
     },
     {
         id: 5,
         name: "Hawkeye",
-        backgroundImage: 'https://github.com/austindickey/memory-game/blob/master/game/public/img/superheros/hawkeye.jpg?raw=true'
+        pic: "/img/superheros/hawkeye.jpg"
     },
     {
         id: 6,
         name: "Hulk",
-        backgroundImage: 'https://github.com/austindickey/memory-game/blob/master/game/public/img/superheros/hulk.jpg?raw=true'
+        pic: "/img/superheros/hulk.jpg"
     },
     {
         id: 7,
         name: "Iron Man",
-        backgroundImage: 'https://github.com/austindickey/memory-game/blob/master/game/public/img/superheros/iron-man.jpg?raw=true'
+        pic: "/img/superheros/iron-man.jpg"
     },
     {
         id: 8,
         name: "Maria Hill",
-        backgroundImage: 'https://github.com/austindickey/memory-game/blob/master/game/public/img/superheros/maria-hill.jpg?raw=true'
+        pic: "/img/superheros/maria-hill.jpg"
     },
     {
         id: 9,
         name: "Nick Fury",
-        backgroundImage: 'https://github.com/austindickey/memory-game/blob/master/game/public/img/superheros/nick-fury.jpg?raw=true'
+        pic: "/img/superheros/nick-fury.jpg"
     },
     {
         id: 10,
         name: "Phil Coulson",
-        backgroundImage: 'https://github.com/austindickey/memory-game/blob/master/game/public/img/superheros/phil-coulson.jpg?raw=true'
+        pic: "/img/superheros/phil-coulson.jpg"
     },
     {
         id: 11,
         name: "Spiderman",
-        backgroundImage: 'https://github.com/austindickey/memory-game/blob/master/game/public/img/superheros/spiderman.jpg?raw=true'
+        pic: "/img/superheros/spiderman.jpg"
     },
     {
         id: 12,
         name: "Thor",
-        backgroundImage: 'https://github.com/austindickey/memory-game/blob/master/game/public/img/superheros/thor.jpg?raw=true'
+        pic: "/img/superheros/thor.jpg"
     }
 ]
 
@@ -84,7 +84,6 @@ class ImageGrid extends React.Component {
             this.setState({ current: 0 })
         } else {
             // Win Logic
-            console.log("You won")
             chosenPics.push(picId)
             this.setState({ current: this.state.current + 1 })
 
@@ -102,8 +101,9 @@ class ImageGrid extends React.Component {
                 <Scores current={this.state.current} best={this.state.best}></Scores>
                 <div id="imageGrid">
 
+                    {/* Mapping images array */}
                     {images.map(item => (
-                        <img className="superheroPic" src={item.backgroundImage} alt="Superhero Pic" key={item.id} id={item.id} onClick={this.picClick}></img>
+                        <img className="superheroPic" src={window.location.origin + item.pic} alt="Superhero Pic" key={item.id} id={item.id} onClick={this.picClick}></img>
                     ))}
                     
                 </div>
--- game/src/components/Navbar.js
+++ game/src/components/Navbar.js
@@ -1,17 +1,10 @@
 import React from "react"
 
-const logo = {
-  backgroundImage: 'url(https://github.com/austindickey/memory-game/blob/master/game/public/img/logo.png?raw=true)',
-  height: 70,
-  width: 324,
-  backgroundRepeat: "no-repeat"
-}
-
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg">
       <a className="logo" href="/">
-        <div role="img" style={logo}></div>
+        <img src={window.location.origin + "/img/logo.png"} alt="Logo"/>
       </a>
     </nav>
   )

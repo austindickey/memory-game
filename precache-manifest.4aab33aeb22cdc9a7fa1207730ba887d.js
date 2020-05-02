5b3a915dacdf5b45dd863b36dd862ad4ad66eb1d
austindickey <austindickey@me.com>
2020-05-02 12:45:29 -0500

    Updates

---
+++ precache-manifest.4aab33aeb22cdc9a7fa1207730ba887d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "012a50693f107fc804f077105a6f6b54",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "5cb07065bc87dce14f15",
+    "url": "/memory-game/static/js/2.da7680f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game/static/js/2.da7680f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e456fdca419aee6cbc0",
+    "url": "/memory-game/static/js/main.79efc3e6.chunk.js"
+  },
+  {
+    "revision": "a0435f9474e61200aa93",
+    "url": "/memory-game/static/js/runtime-main.9e49414f.js"
+  }
+]);
